74f98f1d434deadc0e9188fb83565e6e3c0964ba
David Burke <dburke@amberfire.net>
2020-12-11 11:31:47 -0800

    Day 11

--- day_10.rb
+++ day_10.rb
@@ -20,13 +20,45 @@ class Adapters
         count_of_3 += 1
         return count_of_1 * count_of_3
     end
+
+    def count_arrangements(debug)
+        arrangements = Array.new
+        jolt_with_outlet = @joltages.unshift(0)
+        jolt_with_outlet[1..-1].each do |jolt|
+            cnt = 0
+            cnt +=1 if jolt_with_outlet.include?(jolt-1)
+            cnt +=1 if jolt_with_outlet.include?(jolt-2)
+            cnt +=1 if jolt_with_outlet.include?(jolt-3)
+            puts "#{jolt} - #{cnt}"
+            arrangements.push(cnt) 
+        end
+        return calc_perm(arrangements,debug)
+    end
+
+    def calc_perm(arrangements,debug)
+        idx =0
+        total = 0
+        perms = 1
+        arrangements.each do |count|
+            if count == 1 && perms > 1
+                puts perms
+                total += perms
+                perms =1
+            end
+            if count > 1
+                perms = perms * count
+            end
+        end
+        total
+    end
 end
 
-test = false
-debug = false
+test = true
+debug = false 
 inp = "day_10_input.txt"
 inp = "day_10_test_input.txt" if test
 
 
 ad = Adapters.new(inp)
-puts ad.build_chain(debug)
+puts ad.build_chain(false)
+puts ad.count_arrangements(debug)
--- day_11.rb
+++ day_11.rb
@@ -11,32 +11,37 @@ class Lounge
         File.open(file).each {|row| @seats.push(row.split'')}
         @row_count = @seats.size
         @column_count = @seats[0].size - 1
-        @round = 0
     end
 
-    def follow_seating_rules(debug)
+    def follow_seating_rules(type, debug)
+        @previous = []
+        @round = 0
         until @previous == @seats do
-            do_round
+            do_round(type,debug)
             show_state if debug
         end
         return @seats.flatten.select{|seat| seat==OCCUPIED}.count
     end
 
-    def do_round
+    def do_round(type,debug)
         @previous = @seats.clone.map(&:clone)
         @round += 1
         (0...@row_count).each do |y|
             (0...@column_count).each do |x|
-                @seats[y][x] = new_state(y,x) unless @seats[y][x] == FLOOR
+                if type == 1
+                    seats[y][x] = new_state_rule1(y,x,debug) unless @seats[y][x] == FLOOR
+                else
+                    seats[y][x] = new_state_rule2(y,x,debug) unless @seats[y][x] == FLOOR
+                end
             end
         end
     end
 
-    def new_state(y,x)
+    def new_state_rule1(y,x,debug)
         empty_cnt = 0
         (y-1..y+1).each do |yy|
             (x-1..x+1).each do |xx|
-                if yy < 0 || yy == @row_count || xx < 0 || xx == @column_count
+                if out_of_bounds(yy,xx)
                     empty_cnt += 1
                 else
                     empty_cnt +=1 unless @previous[yy][xx] == OCCUPIED || (yy == y && xx == x)
@@ -45,7 +50,42 @@ class Lounge
         end
         return OCCUPIED if empty_cnt == 8
         return EMPTY if empty_cnt < 5
-        return @seats[y][x]
+        return @previous[y][x]
+    end
+
+    def new_state_rule2(y,x,debug)
+        full_cnt = 0
+        (-1..+1).each do |dy|
+            (-1..+1).each do |dx|
+                unless (dx == 0 && dy == 0) 
+                    full_cnt += 1 if seat_is_occupied(y,x,dy,dx)
+                    puts "#{x},#{y} : #{full_cnt}" if debug && @round ==2 
+                end
+            end
+        end
+        return OCCUPIED if full_cnt == 0
+        return EMPTY if full_cnt > 4
+        return @previous[y][x]
+    end
+
+    def seat_is_occupied(y,x,dy,dx)
+        chk_y = y + dy
+        chk_x = x + dx
+        obj = FLOOR
+        
+        until out_of_bounds(chk_x,chk_y) || @previous[chk_y][chk_x] != FLOOR do
+            chk_y += dy
+            chk_x += dx
+        end
+        return false if out_of_bounds(chk_x,chk_y)
+        return true if  @previous[chk_y][chk_x] == OCCUPIED
+        return false
+    end
+
+    def out_of_bounds(x,y)
+        return true if x < 0 || x == @column_count
+        return true if y < 0 || y == @row_count
+        return false
     end
 
     def show_state
@@ -67,4 +107,4 @@ inp = "day_11_input.txt"
 inp = "day_11_test_input.txt" if test
 
 lng=Lounge.new(inp)
-puts lng.follow_seating_rules(debug)
+puts lng.follow_seating_rules(2, debug)
